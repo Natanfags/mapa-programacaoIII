2c02c7031e3c0b00fd08829fe29d56bb0077c979
Natan Fagundes <natan.fags@gmail.com>
2019-08-15 10:29:01 -0300

    implementado classe carro

--- 17535715/Model/Carro.php
+++ 17535715/Model/Carro.php
@@ -1,3 +1,38 @@
 <?php
-    var_dump($_POST);
-    
+class Carro{
+
+    private $idCarro, $idVeiculo, $qtdPassageiros, $numPortas;
+
+    public function setIdCarro($idCarro){
+        $this->idCarro=$idCarro;
+    }
+
+    public function getIcarro(){
+        return $this->idCarro;
+    }
+
+    public function setIdVeiculo($idVeiculo){
+        $this->idVeiculo=$idVeiculo;
+    }
+
+    public function getidVeiculo(){
+        return $this->idVeiculo;
+    }
+
+    public function setQtdPassageiros($qtdPassageiros){
+        $this->qtdPassageiros=$qtdPassageiros;
+    }
+
+    public function getQtdPassageiros(){
+        return $this->qtdPassageiros;
+    }
+
+    public function setNumPortas($numPortas){
+        $this->numPortas=$numPortas;
+    }
+
+    public function getNumPortas(){
+        return $this->numPortas;
+    }
+}
+?>
